bbe43525d0b1fc256b73014c2f9bf5baeecc9e65
Mitali Rote <mitali.rote@coditas.com>
2025-10-17 11:59:23 +0530

    Updated Comments Datasource

--- social_media_clone/lib/data/datasources/remote/comments_remote_datasource.dart
+++ social_media_clone/lib/data/datasources/remote/comments_remote_datasource.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:social_media_clone/core/constants/api_constants.dart';
 import 'package:social_media_clone/core/network/dio_client.dart';
 import 'package:social_media_clone/data/models/comment_model.dart';
 import 'package:social_media_clone/presentation/features/home/provider/home_provider.dart';
@@ -16,7 +17,7 @@ class CommentRemoteDataSource {
 
   
   Future<CommentModel> createComment(int postId, String content) async {
-    final response = await dioClient.post('/api/comments/post/$postId', {
+    final response = await dioClient.post(ApiConstants.createComment, {
       'content': content,
     });
     return CommentModel.fromJson(response.data);
@@ -24,14 +25,14 @@ class CommentRemoteDataSource {
 
 
   Future<List<CommentModel>> getCommentsForPost(int postId) async {
-    final response = await dioClient.get('/api/comments/post/$postId');
+    final response = await dioClient.get(ApiConstants.getCommentsForPost);
     final data = response.data as List;
     return data.map((e) => CommentModel.fromJson(e)).toList();
   }
 
 
   Future<CommentModel> editComment(int commentId, String newContent) async {
-    final response = await dioClient.put('/api/comments/update/{id}', {
+    final response = await dioClient.put(ApiConstants.editComment, {
       'content': newContent,
     });
     return CommentModel.fromJson(response.data);
@@ -39,18 +40,18 @@ class CommentRemoteDataSource {
 
  
   Future<void> deleteComment(int commentId) async {
-    await dioClient.delete('/api/comments/delete/{id}');
+    await dioClient.delete(ApiConstants.deleteComment);
   }
 
   Future<List<CommentModel>> getAllComments() async {
-    final response = await dioClient.get('/api/comments/post/approved/{postId}');
+    final response = await dioClient.get(ApiConstants.getUserComments);
     final data = response.data as List;
     return data.map((e) => CommentModel.fromJson(e)).toList();
   }
 
  
   Future<List<CommentModel>> getUserComments() async {
-    final response = await dioClient.get('/api/comments/profile');
+    final response = await dioClient.get(ApiConstants.getUserComments);
     final data = response.data as List;
     return data.map((e) => CommentModel.fromJson(e)).toList();
   }
